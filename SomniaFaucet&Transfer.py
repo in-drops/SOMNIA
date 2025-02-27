9877628347432df1e9b38d79ac923fbbef899526
in_drops <indropschannel@gmail.com>
2025-02-27 03:09:23 +0300

    commits 27.02.25

---
+++ SomniaFaucet&Transfer.py
@@ -0,0 +1,119 @@
+
+import random
+from loguru import logger
+from config import config, Chains
+from core.bot import Bot
+from core.excel import Excel
+from models.account import Account
+from utils.inputs import input_pause, input_cycle_pause, input_cycle_amount
+from utils.logging import init_logger
+from utils.utils import (random_sleep, get_accounts, select_profiles)
+
+
+def main():
+
+    if not config.is_browser_run:
+        config.is_browser_run = True
+    init_logger()
+    accounts = get_accounts()
+    accounts_for_work = select_profiles(accounts)
+    pause = input_pause()
+    cycle_amount = input_cycle_amount()
+    cycle_pause = input_cycle_pause()
+
+    for i in range(cycle_amount):
+        random.shuffle(accounts_for_work)
+        for account in accounts_for_work:
+            worker(account)
+            random_sleep(pause)
+
+        logger.success(f'Цикл {i + 1} завершен, обработано {len(accounts_for_work)} аккаунтов!')
+        logger.info(f'Ожидание перед следующим циклом ~{config.pause_between_cycle[1]} секунд!')
+        random_sleep(cycle_pause)
+
+def worker(account: Account) -> None:
+
+    try:
+        with Bot(account) as bot:
+            activity(bot)
+    except Exception as e:
+        logger.critical(f"{account.profile_number} Ошибка при инициализации Bot: {e}")
+
+def activity(bot: Bot):
+
+    excel_report = Excel(bot.account, file='SonniaActivity.xlsx')
+    excel_report.set_cell('Address', f'{bot.account.address}')
+    excel_report.set_date('Date')
+    bot.metamask.auth_metamask()
+    bot.metamask.select_chain(Chains.SOMNIA_TESTNET)
+    bot.ads.open_url('https://testnet.somnia.network')
+    random_sleep(5, 10)
+    connect_button = bot.ads.page.get_by_role('button', name='Connect')
+    if connect_button.count():
+        bot.ads.page.get_by_role('button', name='Connect').click()
+        random_sleep(2, 3)
+        bot.ads.page.get_by_text('MetaMask').click()
+        bot.metamask.universal_confirm()
+        random_sleep(2, 3)
+
+    if bot.ads.page.locator('.text-green-500', has_text='Live').count():
+        pass
+    else:
+        logger.error('Сайт на данный момент не активен! Попробуйте позже.')
+        return
+
+    for _ in range(3):
+        bot.ads.page.reload(wait_until='load')
+        random_sleep(2, 3)
+        bot.ads.page.get_by_role('button', name='Request Tokens').scroll_into_view_if_needed()
+        random_sleep(3, 5)
+        bot.ads.page.get_by_role('button', name='Request Tokens').hover(timeout=5000)
+        bot.ads.page.get_by_role('button', name='Request Tokens').click()
+        random_sleep(3, 5)
+        bot.ads.page.get_by_role('button', name='Get STT').hover()
+        bot.ads.page.get_by_role('button', name='Get STT').click()
+        random_sleep(5, 10)
+        if bot.ads.page.locator('button', has_text='Tokens Sent!').count():
+            logger.success('Токены STT успешно получены! Делаем transfer.')
+            bot.ads.page.locator('button', has_text='Close').click()
+            excel_report.increase_counter(f'Faucet')
+            random_sleep(5, 10)
+            break
+    else:
+        logger.warning('Ошибка получения токенов STT! Делаем transfer.')
+        bot.ads.page.locator('button', has_text='Close').click()
+
+    random_sleep(5, 10)
+    bot.ads.page.get_by_role('button', name='Send Tokens').hover(timeout=5000)
+    bot.ads.page.get_by_role('button', name='Send Tokens').click()
+    random_sleep(3, 5)
+
+    bot.ads.page.get_by_role('button', name='0.001').hover(timeout=5000)
+    bot.ads.page.get_by_role('button', name='0.001').click()
+    random_sleep(3, 5)
+
+    bot.ads.page.get_by_role('button', name='Random Address').hover(timeout=5000)
+    bot.ads.page.get_by_role('button', name='Random Address').click()
+    random_sleep(3, 5)
+
+    bot.ads.page.get_by_role('button', name='Send STT').hover(timeout=5000)
+    bot.ads.page.get_by_role('button', name='Send STT').click()
+    bot.metamask.universal_confirm()
+    random_sleep(10, 15)
+
+    bot.ads.page.get_by_role('button', name='Send Tokens').hover(timeout=5000)
+    bot.ads.page.get_by_role('button', name='Send Tokens').click()
+
+    if bot.ads.page.get_by_role('dialog').get_by_text('Transfer successful!').count():
+        logger.success('Transfer токенов произведён успешно! Данные записаны в таблицу SomniaActivity.xlsx')
+        excel_report.increase_counter(f'Transfer Site')
+    else:
+        logger.error('Transfer токенов не удался! Возможно нехватка баланса или ошибка сайта / блокчейна.')
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.warning('Программа завершена вручную!')
---
+++ SomniaTransferToAddress.py
@@ -0,0 +1,74 @@
+
+import random
+import time
+from loguru import logger
+from config import config, Chains, Tokens
+from core.bot import Bot
+from core.excel import Excel
+from core.onchain import Onchain
+from models.account import Account
+from models.amount import Amount
+from snippets.activities.relay import relay
+from utils.inputs import input_pause, input_cycle_pause, input_cycle_amount
+from utils.logging import init_logger
+from utils.utils import (random_sleep, get_accounts, select_profiles, generate_random_evm_address, get_user_agent)
+
+
+def main():
+
+    init_logger()
+    accounts = get_accounts()
+    accounts_for_work = select_profiles(accounts)
+    pause = input_pause()
+    cycle_amount = input_cycle_amount()
+    cycle_pause = input_cycle_pause()
+    for i in range(cycle_amount):
+        random.shuffle(accounts_for_work)
+        for account in accounts_for_work:
+            worker(account)
+            random_sleep(pause)
+        logger.success(f'Цикл {i + 1} завершен, обработано {len(accounts_for_work)} аккаунтов!')
+        logger.info(f'Ожидание перед следующим циклом ~{config.pause_between_cycle[1]} секунд!')
+        random_sleep(cycle_pause)
+
+def worker(account: Account) -> None:
+
+    try:
+        with Bot(account) as bot:
+            activity(bot)
+    except Exception as e:
+        logger.critical(f"{account.profile_number} Ошибка при инициализации Bot: {e}")
+
+def activity(bot: Bot):
+
+    get_user_agent()
+    bot.onchain.change_chain(Chains.SOMNIA_TESTNET)
+    excel_report = Excel(bot.account, file='SonniaActivity.xlsx')
+    excel_report.set_cell('Address', f'{bot.account.address}')
+    excel_report.set_date('Date')
+    random_address = generate_random_evm_address()
+    random_amount = Amount(random.uniform(0.001, 0.01))
+    balance_before = bot.onchain.get_balance().ether
+    if balance_before < (random_amount + 0.001):
+        logger.error('Баланс недостаточный или слишком маленький! Сделайте пополнение')
+    tx_params = bot.onchain._prepare_tx(value=random_amount, to_address=random_address)
+    tx_params = bot.onchain._estimate_gas(tx_params)
+    tx_hash = bot.onchain._sign_and_send(tx_params)
+    for _ in range(60):
+        balance_after = bot.onchain.get_balance().ether
+        if balance_after < balance_before:
+            excel_report.increase_counter(f'Transfer Web3')
+            logger.success(f'Transfer токенов произведён успешно! Данные записаны в таблицу SomniaActivity.xlsx. Hash: {tx_hash}')
+            break
+        random_sleep(5, 10)
+    else:
+        logger.error('Transfer токенов не удался!')
+        raise Exception('Transfer токенов не удался!')
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.warning('Программа завершена вручную!')
